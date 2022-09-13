5c9385a1ec48af0b15b50112479a375d2e8941ac
673799093@qq.com <673799093@qq.com>
2022-09-13 23:14:34 +0900

    repeat update and standardlibrary create

--- repeat.py
+++ repeat.py
@@ -1,5 +1,6 @@
 #! usr/bin/python
 
+# repeat remove
 my_list = [1,1,3,3,4,5,6]
 my_set = set(my_list)
 print(my_list)
---
+++ standardlibrary.py
@@ -0,0 +1,21 @@
+#! usr/bin/python
+
+# os, re, pickle, datetime, time, math
+import os
+import re
+import pickle
+import datetime
+import time 
+import math
+
+def ostest():
+    a = os.access
+    b = os.getlogin()
+    print(b)
+    print(a)
+def retest():
+    print("test")
+
+if __name__ == "__main__":
+    # ostest()
+    retest()
